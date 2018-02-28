8022ca8fd6e9e6ce82cf74a8f2517373b6ffe4f9
Kenneth Reitz <me@kennethreitz.org>
2018-02-28 08:30:02 -0500

    v0.1.0
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ required = [
 
 setup(
     name='delegator.py',
-    version='0.0.14',
+    version='0.1.0',
     description='Subprocesses for Humans 2.0.',
     long_description=long_description,
     author='Kenneth Reitz',
