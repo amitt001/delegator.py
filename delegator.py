06e44ab0885a3b36a0d380f05927f2017652ed1f
Kenneth Reitz <me@kennethreitz.org>
2018-02-28 08:33:28 -0500

    pid exists
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- delegator.py
+++ delegator.py
@@ -15,6 +15,33 @@ except NameError:
 
 TIMEOUT = 30
 
+def pid_exists(pid):
+    """Check whether pid exists in the current process table."""
+    if pid == 0:
+        # According to "man 2 kill" PID 0 has a special meaning:
+        # it refers to <<every process in the process group of the
+        # calling process>> so we don't want to go any further.
+        # If we get here it means this UNIX platform *does* have
+        # a process with id 0.
+        return True
+    try:
+        os.kill(pid, 0)
+    except OSError as err:
+        if err.errno == errno.ESRCH:
+            # ESRCH == No such process
+            return False
+        elif err.errno == errno.EPERM:
+            # EPERM clearly means there's a process to deny access to
+            return True
+        else:
+            # According to "man 2 kill" possible error values are
+            # (EINVAL, EPERM, ESRCH) therefore we should never get
+            # here. If we do let's be explicit in considering this
+            # an error.
+            raise err
+    else:
+        return True
+
 class Command(object):
 
     def __init__(self, cmd, timeout=TIMEOUT):
@@ -124,6 +151,11 @@ class Command(object):
             return self.subprocess.proc.pid
         # Standard subprocess method.
         return self.subprocess.pid
+
+    @property
+    def is_alive(self):
+        """Is the process alive?"""
+        return pid_exists(self.pid)
 
     @property
     def return_code(self):
